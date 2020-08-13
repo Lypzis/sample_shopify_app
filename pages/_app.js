c0ce915778c869fbb783026c41798aaf4e244258
Lypzis <victorvp2018@protonmail.com>
2020-08-12 21:01:12 -0300

    Update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css';
+import React, { Fragment } from 'react';
+import App from 'next/app';
+import Head from 'next/head';
+import { AppProvider } from '@shopify/polaris';
+import translations from '@shopify/polaris/locales/en.json';
+import '@shopify/polaris/dist/styles.css';
 
 const MyApp = ({ Component, pageProps }) => {
-	return <Component {...pageProps} />;
+	return (
+		<Fragment>
+			<Head>
+				<title>Sample Shopify App</title>
+				<meta charSet='utf-8' />
+			</Head>
+			<AppProvider il8n={translations}>
+				<Component {...pageProps} />
+			</AppProvider>
+		</Fragment>
+	);
 };
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
-import styles from '../styles/Home.module.css';
+import { TextStyle, Page, Layout, EmptyState } from '@shopify/polaris';
+
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
 const Index = props => {
 	return (
-		<div className={styles.container}>
-			<p>Sample app using React and Next.js</p>
-		</div>
+		<Page>
+			<Layout>
+				{/* <TextStyle variation='positive'>
+					Sample app using React and Next.js
+				</TextStyle> */}
+				<EmptyState
+					heading='Discount your prices temporarily'
+					action={{
+						content: 'Select products',
+						onAction: () => console.log('clicked :D'),
+					}}
+					image={img}>
+					<p>Select products to change their price temporarily.</p>
+				</EmptyState>
+			</Layout>
+		</Page>
 	);
 };
 
